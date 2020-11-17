ed6e90199f5c5ecf16e239319dda1b39de9eb843
Violeta <violetapopans@gmail.com>
2020-11-17 10:57:41 +0100

    added petlja for  and do while

--- G7Cas21/Program.cs
+++ G7Cas21/Program.cs
@@ -9,19 +9,22 @@ namespace G7Cas21
         {
             //*** while
             int a = 10;
-            while(true)
+            while(a <20)
             {
                 Console.WriteLine(" A = {0}", a);
                 a++;
-                break;
+                
             }
             Console.WriteLine("Out of the loop");
 
             //*** for
-            for(int i =0; i <10; i++)
+            for(int i =0; i <50; i= i + 5)
             {
-
+                Console.WriteLine("for loop  | i = {0}", i);
             }
+
+            // *** do .. while
+
 
             Console.ReadKey();
         }
