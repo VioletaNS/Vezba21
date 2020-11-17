0836c648697872e6845391b97c934fff3ecd6c46
Violeta <violetapopans@gmail.com>
2020-11-17 10:17:01 +0100

    Add project files.

---
+++ G7Cas21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace G7Cas21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*** while
+            int a = 10;
+            while(true)
+            {
+                Console.WriteLine(" A = {0}", a);
+                a++;
+                break;
+            }
+            Console.WriteLine("Out of the loop");
+
+            //*** for
+            for(int i =0; i <10; i++)
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
